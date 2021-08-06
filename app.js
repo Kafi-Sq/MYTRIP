c1a15127bc96e97c54f2953575302735234cfebb
Hassen Hassen <xasansq@gmail.com>
2021-08-05 23:28:53 -0500

    fixed

--- app.js
+++ app.js
@@ -113,7 +113,8 @@ app.use((err, req, res, next) => {
 })
 
 // listening 
-app.listen(8080, () => {
-    console.log('Listening: 8080')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
 })
 /* final commit */
