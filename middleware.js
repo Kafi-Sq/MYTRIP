dba51880baa6c18ffc05ebb35520f5acadb515b8
Hassen Hassen <xasansq@gmail.com>
2021-08-06 08:24:47 -0500

    fixed headers bug

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()){
         req.session.returnTo = req.originalUrl
         req.flash('error', 'You must be logged in!')
-        res.redirect('/login')
+        return res.redirect('/login')
     }
     next()
 }
